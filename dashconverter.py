a469f9c7f421b8a27a5b8b9043e443fb49f8ddb0
Steve Rice <steve.rice@brigade.com>
2017-06-28 23:34:12 -0700

    Add ability to swap out one service name for another in metrics

--- dashconverter.py
+++ dashconverter.py
@@ -1,9 +1,10 @@
 from datadog import initialize, api
 import sys
+import os
 
 options = {
-	'api_key': '****',
-	'app_key': '****'
+	'api_key': os.environ['DD_API_KEY'],
+	'app_key': os.environ['DD_APP_KEY']
 }
 initialize(**options)
 
@@ -86,7 +87,7 @@ class converter(object):
 
 		## Convert the widgets
 	@classmethod
-	def convert_s2t(cls, widgets):
+	def convert_s2t(cls, widgets, old_service = None, new_service = None):
 		# Function to convert Screenboard to Timeboard.
 		# Takes the widgets as input and output the widgets properly formatted.
 		# Appens an additionnal attribute for the hostmap.
@@ -101,6 +102,12 @@ class converter(object):
 				widgets[i]['tile_def'] = 'outdated'
 				print "One of the widgets' type is outdated and won't be ported.\n To solve this, just click on edit the dashboard, open a widget, hit done and save the dashboard.\n Then run the script again."
 				print widgets[i]
+
+
+			# Replace service name in metrics
+			if old_service and new_service:
+				for request in widgets[i]['tile_def']['requests']:
+					request['q'] = request['q'].replace(old_service, new_service)
 
 			if widgets[i]['type'] == 'hostmap':
 
@@ -126,7 +133,7 @@ class converter(object):
 
 		## Convert the widgets
 	@classmethod
-	def convert_t2s(cls, graphs):
+	def convert_t2s(cls, graphs, old_service = None, new_service = None):
 		# Function to convert Timeboard to Screenboard.
 		# Takes the widgets as input and output the widgets properly formatted.
 		# pos_x, pos_y and tmp_y assure the right position of the widgets
@@ -140,6 +147,11 @@ class converter(object):
 		margin = 5
 		tmp_y = 0
 		for i in range(len(graphs)):
+
+			# Replace service name in metrics
+			if old_service and new_service:
+				for request in graphs[i]['definition']['requests']:
+					request['q'] = request['q'].replace(old_service, new_service)
 
 			if i % 2 == 0 and i != 0:
 				pos_x = 0
@@ -203,7 +215,7 @@ class converter(object):
 				})
 
 	@classmethod
-	def main(cls, dash):
+	def main(cls, dash, old_service = None, new_service = None):
 		# Main fuction to fetch the dashboards, extract the widgets, transform the widgets and push the result
 		# Takes the dash to convert as an input
 		# Outputs the url of the new dash
@@ -211,16 +223,19 @@ class converter(object):
 		cls.getdash(dash)
 		if cls.board_type == "screenboard":
 			widgets = cls.widget_transform()
-			cls.convert_s2t(widgets)
+			cls.convert_s2t(widgets, old_service, new_service)
 			output = api.Timeboard.create(title=cls.title, description='description', graphs=cls.graphs, template_variables=cls.template_variables, read_only=False)
 			cls.delete_dash(dash)
 			print 'Your new Timeboard is available at: http://app.datadoghq.com'+output['url']
 
 		else:
 			graphs = cls.widget_transform()
-			cls.convert_t2s(graphs)
+			cls.convert_t2s(graphs, old_service, new_service)
 			output = api.Screenboard.create(board_title=cls.title, description='description', widgets=cls.widgets, template_variables=cls.template_variables)			
 			cls.delete_dash(dash)			
 			print "Your new Screenboard is available at: http://app.datadoghq.com/screen/" + str(output['id'])
+if len(sys.argv) == 4:
+	converter().main(sys.argv[1], sys.argv[2], sys.argv[3])
+else:
+	converter().main(sys.argv[1])
 
-converter().main(sys.argv[1])
